c69e5e210572b9e8f463def8c17c5fe8b1ff5f01
Anton Chyrko <anton_chyrko@epam.com>
2021-02-28 13:12:28 +0000

    Calculator dev

---
+++ src/main/java/com/epam/javabasic/calc/ICalculator.java
@@ -0,0 +1,17 @@
+package com.epam.javabasic.calc;
+
+/**
+ * Please, see readme.md file! All detailed instructions are laid there.
+ *
+ * Created by anton_chyrko on 12/21/20.
+ */
+public interface ICalculator {
+
+	double add(double a, double b);
+
+	double subtract(double a, double b);
+
+	double multiply(double a, double b);
+
+	double divide(double a, double b);
+}
