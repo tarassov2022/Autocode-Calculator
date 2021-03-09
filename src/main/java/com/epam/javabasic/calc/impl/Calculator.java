fe0075fe693d294d21ade9b7f7c17ca23fa42b0e
Anton_Chyrko <Anton_Chyrko@epam.com>
2021-03-09 17:20:14 +0300

    add implementation for ICalculator

---
+++ src/main/java/com/epam/javabasic/calc/impl/Calculator.java
@@ -0,0 +1,35 @@
+package com.epam.javabasic.calc.impl;
+
+import com.epam.javabasic.calc.ICalculator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calculator implements ICalculator {
+
+    private int precision;
+
+    public Calculator(int precision) {
+        this.precision = precision;
+    }
+
+    @Override
+    public double add(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    @Override
+    public double subtract(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    @Override
+    public double divide(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+}
